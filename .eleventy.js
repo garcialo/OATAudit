9adec9f35f5b854953b562ea5561b327e2e7036a
garcialo <github@garcialo.com>
2023-06-16 22:43:58 -0700

    adding local version of dexie.min.js
    Added minified Dexie library and updated 11ty config to pass it through to the generated site.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("dexie.min.js");
 	eleventyConfig.addPassthroughCopy("index.js");
 	eleventyConfig.addPassthroughCopy("audit.js");
 };
