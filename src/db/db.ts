db385aecbb4c4e79e9614086924085670499b427
garcialo <github@garcialo.com>
2023-08-11 20:45:19 -0700

    Changing up the default audit, page, page states, and issues

--- src/db/db.ts
+++ src/db/db.ts
@@ -35,140 +35,70 @@ export const db = new AuditDB();
 db.on("populate", async () => {
 	db.audits.bulkPut([
 		{
-			id: 30,
-			name: "Example COM and ORG",
-			checklist_ID: 301,
-			page_IDs: [400, 401],
-			issue_IDs: [201, 202, 203, 204, 205, 206, 207],
-		},
-		{
-			id: 31,
-			name: "Only example.com",
-			checklist_ID: 301,
-			page_IDs: [402],
-			issue_IDs: [],
+			id: 1,
+			name: "Sample Audit",
+			checklist_ID: 1,
+			page_IDs: [1],
+			issue_IDs: [1, 2, 3, 4],
 		},
 	]);
 
 	db.checklists.bulkPut(all_checklists);
 
-	db.checklists.bulkPut([
-		{ id: 300, check_IDs: ["log-1000"], name: "One check checklist" },
-		{
-			id: 301,
-			check_IDs: ["log-1000", "log-1001"],
-			name: "Two check checklist",
-		},
-		{ id: 2, check_IDs: ["log-1001"], name: "Only 1001 - default" },
-	]);
-
 	db.issues.bulkPut([
 		{
-			id: 200,
-			check_ID: "log-1000",
+			id: 1,
+			check_ID: "oat0001",
 			status: "Pass",
-			page_state_ID: 100,
-			page_ID: 400,
-			issue_number: 1,
+			page_state_ID: 1,
+			page_ID: 1,
 		},
 		{
-			id: 201,
-			check_ID: "log-1001",
+			id: 2,
+			check_ID: "oat0002",
 			status: "Pass",
-			page_state_ID: 100,
-			page_ID: 400,
-			issue_number: 2,
+			page_state_ID: 1,
+			page_ID: 1,
 		},
 		{
-			id: 202,
-			check_ID: "log-1000",
-			status: "Pass",
-			page_state_ID: 101,
-			page_ID: 400,
-			issue_number: 3,
-		},
-		{
-			id: 203,
-			check_ID: "log-1001",
+			id: 3,
+			check_ID: "oat0001",
 			status: "Fail",
-			page_state_ID: 101,
-			page_ID: 400,
-			description: "Example description for issue 203",
-			issue_number: 4,
+			page_state_ID: 2,
+			page_ID: 1,
+			description: "Picture ()",
 		},
 		{
-			id: 204,
-			check_ID: "log-1000",
+			id: 4,
+			check_ID: "oat0002",
 			status: "Fail",
-			page_state_ID: 102,
-			page_ID: 401,
-			description: "Example description for issue 204",
-			issue_number: 5,
-		},
-		{
-			id: 205,
-			check_ID: "log-1001",
-			status: "Fail",
-			page_state_ID: 102,
-			page_ID: 401,
-			description: "Example description for issue 205",
-			issue_number: 6,
-		},
-		{
-			id: 206,
-			check_ID: "log-1000",
-			status: "Pass",
-			page_state_ID: 103,
-			page_ID: 401,
-			issue_number: 7,
-		},
-		{
-			id: 207,
-			check_ID: "log-1001",
-			status: "Fail",
-			page_state_ID: 103,
-			page_ID: 401,
-			description: "Example description for issue 207",
-			issue_number: 8,
+			page_state_ID: 2,
+			page_ID: 1,
+			description:
+				'Image link (<a href="homepage.html"<img src="home.png"></a>) missing text alternative',
 		},
 	]);
 
 	db.page_states.bulkPut([
-		{ id: 100, name: "Initial", page_ID: 400 },
 		{
-			id: 101,
-			name: "Fake Page",
-			page_ID: 400,
-			instructions: "Sample instructions for page 400",
+			id: 1,
+			name: "Initial",
+			page_ID: 1,
 		},
-		{ id: 102, name: "Initial", page_ID: 401 },
 		{
-			id: 103,
-			name: "Made up page",
-			page_ID: 401,
-			instructions: "Sample instructions for page 401",
+			id: 2,
+			name: "Sample Page State",
+			page_ID: 1,
+			instructions: "Click 'X' close button to hide the consent dialog",
 		},
-		{ id: 104, name: "Initial", page_ID: 402 },
 	]);
 
 	db.pages.bulkPut([
 		{
-			id: 400,
-			name: "Homepage COM",
+			id: 1,
+			name: "Sample Page",
 			url: "example.com",
-			page_state_IDs: [100, 101],
-		},
-		{
-			id: 401,
-			name: "Homepage ORG",
-			url: "example.org",
-			page_state_IDs: [102, 103],
-		},
-		{
-			id: 402,
-			name: "Example COM Homepage",
-			url: "example.com",
-			page_state_IDs: [104],
+			page_state_IDs: [1, 2],
 		},
 	]);
 
