3afceb6d2c35a395e7370e39626682fcca5e0a98
garcialo <github@garcialo.com>
2023-08-12 06:51:21 -0700

    Adding Oat WCAG checklists array used for db

--- src/data/importChecklists.ts
+++ src/data/importChecklists.ts
@@ -1,5 +1,7 @@
 // Putting the checklists into the DB is done by /db/db.ts
 import { Checklist } from "../interfaces";
 import { oatMinimum } from "./checklists/oatMinimum";
+import { oatWcag20 } from "./checklists/oatWcag20";
+import { oatWcag21 } from "./checklists/oatWcag21";
 
-export const all_checklists: Checklist[] = [oatMinimum];
+export const all_checklists: Checklist[] = [oatMinimum, oatWcag20, oatWcag21];
