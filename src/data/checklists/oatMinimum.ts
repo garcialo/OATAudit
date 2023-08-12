634c2397f45f51252743c7bea7a14371103333d1
garcialo <github@garcialo.com>
2023-08-11 20:44:30 -0700

    Adding checks to "OAT Minimum" checklist

--- src/data/checklists/oatMinimum.ts
+++ src/data/checklists/oatMinimum.ts
@@ -3,5 +3,22 @@ import { Checklist } from "../../interfaces";
 export const oatMinimum: Checklist = {
 	id: 1,
 	name: "OAT Minimum",
-	check_IDs: ["oat0001"],
+	check_IDs: [
+		"oat0001",
+		"oat0002",
+		"oat0003",
+		"oat0004",
+		"oat0041",
+		"oat0055",
+		"oat0056",
+		"oat0057",
+		"oat0058",
+		"oat0059",
+		"oat0064",
+		"oat0071",
+		"oat0072",
+		"oat0073",
+		"oat0074",
+		"oat0079",
+	],
 };
